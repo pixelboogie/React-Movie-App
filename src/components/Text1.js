52d9c48a6b6d899d49a1eff5d2ea6025d78d6382
Shane Anderson <shane@pixelboogie.com>
2019-12-12 14:09:55 -0800

    Got logo on page

---
+++ src/components/Text1.js
@@ -0,0 +1,11 @@
+import React, {Components} from 'react';
+
+class Text1 extends Component {
+    render(){
+        return (
+            <p>Hello from Text1</p>
+        )
+    }
+}
+
+export default Text1;
